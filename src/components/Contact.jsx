ee8db20543e7e2a2f4d4b2b9c82e4bf30b4b2eee
vishwa103 <123248932+vishwa103@users.noreply.github.com>
2024-03-01 17:14:01 +0530

    required fields

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -67,13 +67,13 @@ const Contact = () => {
         <h3 className={styles.sectionHeadText}>Contact.</h3>
         <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
           <label className="flex flex-col "><span className="text-white font-medium mb-4">Your Name</span>
-          <input type="text" name="name" value={form.name} onChange={handleChange} placeholder="What's your name?" className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
+          <input type="text" name="name" value={form.name} onChange={handleChange} placeholder="What's your name?"  required className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
           </label>
           <label className="flex flex-col "><span className="text-white font-medium mb-4">Your Email</span>
-          <input type="email" name="email" value={form.email} onChange={handleChange} placeholder="What's your email?" className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
+          <input type="email" name="email" value={form.email} onChange={handleChange} placeholder="What's your email?" required className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
           </label>
           <label className="flex flex-col "><span className="text-white font-medium mb-4">Your Message</span>
-          <textarea rows="7" name="message" value={form.message} onChange={handleChange} placeholder="What do you want to say?" className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
+          <textarea rows="7" name="message" value={form.message} onChange={handleChange} placeholder="What do you want to say?" required className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
           </label>
           <button type="submit" className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"> {loading ? 'Sending...':'Send'} </button>
         </form>
