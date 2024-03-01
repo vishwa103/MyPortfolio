3248dcba96dad22fb00cce2286965444e2025588
vishwa103 <123248932+vishwa103@users.noreply.github.com>
2024-03-01 10:46:41 +0530

    animation optimization

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -62,9 +62,10 @@ const ComputersCanvas = () =>{
     gl={{preserveDrawingBuffer:true}}
     >
       <Suspense fallback={<CanvasLoader/>}>
-        
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2}/>
-        <Computers isMobile={isMobile}/>
+        <Suspense fallback={<CanvasLoader />}>
+          <Computers isMobile={isMobile} />
+        </Suspense>
       </Suspense>
       <Preload all/>
     </Canvas>
